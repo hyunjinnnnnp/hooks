b0712afcd874b4f2fdf9a67486c8f5119a17dfef
hyunjin <imhjinnnnn@gmail.com>
2021-06-19 16:30:47 +0900

    useTitle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from "react";
 
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    // === <title>
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  //title이 업데이트되면 updateTitle()
+  return setTitle;
+};
+
 const App = () => {
-  const sayHello = () => console.log("hello");
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
-  useEffect(sayHello, [number]);
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div className="App">
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
+      <div>Hi</div>
     </div>
   );
 };
